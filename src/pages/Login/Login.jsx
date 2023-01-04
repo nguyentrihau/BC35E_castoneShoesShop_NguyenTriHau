dc9749a015d44a00cdbb8a26cd9f907b7055ed76
nguyentrihau <nguyenchonhau@gmail.com>
2023-01-04 11:30:52 +0700

    fb

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
               <button className='btn_login'>LOGIN</button>
             </div>
             <div className='' >  <FacebookLogin
-              appId="1111851099506637"
+              appId="1262329041298224"
               autoLoad={false}
               fields="name,email,picture"
               callback={responseFacebook}
